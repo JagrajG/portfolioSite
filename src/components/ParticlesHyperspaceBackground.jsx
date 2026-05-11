fdb79dfbab14bd923d4bec732edb28be5bcddc17
JagrajG <jagrajgill83@gmail.com>
2026-05-11 11:15:33 -0700

    fixed particle issue

--- src/components/ParticlesHyperspaceBackground.jsx
+++ src/components/ParticlesHyperspaceBackground.jsx
@@ -1,30 +1,48 @@
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 import { loadHyperspacePreset } from "@tsparticles/preset-hyperspace";
 
 const ParticlesHyperspaceBackground = () => {
-  const particlesInit = useCallback(async (engine) => {
-    await loadHyperspacePreset(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+      await loadHyperspacePreset(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
+
+  const options = useMemo(
+    () => ({
+      preset: "hyperspace",
+      background: {
+        color: {
+          value: "#ababab",
+        },
+      },
+      particles: {
+        color: {
+          value: "#8f9df0",
+        },
+        move: {
+          speed: 2,
+        },
+      },
+    }),
+    [],
+  );
+
+  if (!init) {
+    return null;
+  }
 
   return (
     <Particles
       id="tsparticles-hyperspace"
-      init={particlesInit}
-      options={{
-        preset: "hyperspace",
-        background: {
-          color: "#ababab",
-        },
-        particles: {
-          color: {
-            value: "#8f9df0",
-          },
-          move: {
-            speed: 2,
-          },
-        },
-      }}
+      options={options}
       style={{
         position: "absolute",
         width: "100%",
